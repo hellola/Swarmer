0bb95de46f7aaf6f6a1069039a48e804f79c858c
hellola <ewoudt.kellerman@gmail.com>
2014-06-10 08:19:30 +1000

    add custom shape as neighbourhood view

--- src/com/evo/componentagent/util/NeighbourhoodData.java
+++ src/com/evo/componentagent/util/NeighbourhoodData.java
@@ -2,15 +2,40 @@ package com.evo.componentagent.util;
 
 import java.util.ArrayList;
 
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Vector2f;
+
 public class NeighbourhoodData {
 	
 	private Integer size; 
 	private String name; 
 	private ArrayList<String> eligibleEntities;
+	private Shape shape; 
+	private Vector2f transformPosition; 
 	
 	public NeighbourhoodData() { 
 		eligibleEntities = new ArrayList<String>(); 
+
 	}
+	
+	
+	public Shape getShape() { 
+		return shape; 
+	}
+	
+	public void setShapeFromString(String shapeName) { 
+		if (shapeName.equals("rectangle")) { 
+			shape = new Rectangle(0,0,60,size); 
+			transformPosition = new Vector2f(-30,0); 
+		}
+		else { 
+			transformPosition = new Vector2f(0,0); 
+			shape = new Circle(0,0,size/2); 
+		}
+	}
+	
 	
 	public ArrayList<String> getEligibleEntities() {
 		return eligibleEntities;
@@ -22,6 +47,7 @@ public class NeighbourhoodData {
 	public void setEligibleEntities(ArrayList<String> filters) {
 		this.eligibleEntities = filters;
 	}
+	
 	public Integer getSize() {
 		return size;
 	}
@@ -33,6 +59,12 @@ public class NeighbourhoodData {
 	}
 	public void setName(String name) {
 		this.name = name;
-	} 
+	}
+
+
+	public Vector2f getTransformPosition() {
+		return transformPosition;
+	}
+
 
 }
